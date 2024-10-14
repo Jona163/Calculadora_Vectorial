bae11c7445878d82de61e9f865b70ba990789220
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-14 12:00:14 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,9 @@
 # Fecha: 14 octubre 2024
 # Descripción: Calculadora Vectorial en Python.
 # GitHub: https://github.com/Jona163
+
+#Librerias importadas de python
+import math
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import numpy as np
