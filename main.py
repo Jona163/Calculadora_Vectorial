6cea887b5e367097bc90eb0f207dbb3d813d9b04
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-14 12:00:27 -0600

    Update main.py

--- main.py
+++ main.py
@@ -8,3 +8,10 @@ import math
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import numpy as np
+
+#Primera parte: Solicitar el número de vectores con el que se va a trabajar.
+print("\033[1;34m"+"\n¡Bienvenido al programa de vectores de XFORCE!"+"\033[0;m"+"\n\n¿Con cuántos vectores (n) deseas trabajar?")
+numvectors=input("\033[1;32m"+"n= "+"\033[0;m")
+while not(numvectors.isdigit()) or not(int(numvectors)>0):
+    print("\033[1;31m"+"**"+"\033[4;30m"+ "Lo sentimos, lo que has ingresado no es un número valido, vuelve a intentarlo"+"\033[0;m")
+    numvectors= input("\033[1;32m"+"n= "+"\033[0;m")
