fa9d84b07bc556ca6da07d177f974858efe2e339
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-15 13:01:47 -0600

    Update main.py

--- main.py
+++ main.py
@@ -385,3 +385,15 @@ for i in range (int(numvectors)):
                     if (puntoFz.split(".")[0].isdigit()) and (puntoFz.split(".")[1].isdigit()):
                         validFz = 1
             if puntoFz.startswith("-"):
+
+                            possFz = puntoFz[1:]
+                if possFz.count(".") != 1:
+                    print("\033[1;31m"+"**"+"\033[4;30m"+"Los enteros y los decimales deben estar separados por un punto."+"\033[0;m")
+                    validFz = 0
+                if possFz.count(".") == 1:
+                    if (not (possFz.split(".")[0].isdigit())) or (not (possFz.split(".")[1].isdigit())):
+                        print("\033[1;31m"+"**"+"\033[4;30m"+"Los elementos antes y después del punto deben ser números."+"\033[0;m")
+                        validFz = 0
+                    if (possFz.split(".")[0].isdigit()) and (possFz.split(".")[1].isdigit()):
+                        validFz = 1
+        infoVectori.append(float(puntoFz))
